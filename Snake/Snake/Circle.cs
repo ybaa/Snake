35c5dbd07e8d2fcb86d4ce55d14d168a41b1d748
Martyna Łagożna <m.lagozna@live.com>
2016-02-04 19:49:44 +0100

    Class added

---
+++ Snake/Snake/Circle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Circle
+    {
+        private int X { set; get; }
+        private int Y { set; get; }
+
+        public Circle()
+        {
+            X = 0;
+            Y = 0;
+        }
+    }
+}
